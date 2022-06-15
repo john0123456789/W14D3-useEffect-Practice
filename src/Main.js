34fc89308f855a07357e58492877dbd7fd851b0b
john0123456789 <johnluongpham@yahoo.com>
2022-06-14 23:32:03 -0700

    useEffect rendering

---
+++ src/Main.js
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import React, { useState } from "react";
+
+const Main = () => {
+  const [toggleOne, setToggleOne] = useState(false);
+  const [toggleTwo, setToggleTwo] = useState(false);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("UseEffect1 Ran");
+  }, []);
+
+  useEffect(() => {
+    console.log("UseEffect2 Ran");
+    if (toggleTwo)
+      console.log(`toggleTwo slice of state is true so this code runs`);
+  }, [toggleTwo]);
+
+  useEffect(() => {
+    const myInterval = setInterval(() => {
+      return () => {
+        console.log(
+          `UseEffect3 cleanup ran.\nsetInterval number ${count} is being cleared out`
+        );
+        clearInterval(myInterval);
+        console.log(`UseEffect3 with interval number ${count} is running`);
+      };
+    }, 1000);
+  }, [count]);
+
+  return (
+    <div>
+      {console.log("rendered or re-rendered")}
+      <h1>Main Component</h1>
+      <button onClick={() => setToggleOne(!toggleOne)}>ToggleOne</button>
+      <button onClick={() => setToggleTwo(!toggleTwo)}>ToggleTwo</button>
+      <button onClick={() => setCount(count + 1)}>Counter</button>
+      {count}
+    </div>
+  );
+};
+
+export default Main;
